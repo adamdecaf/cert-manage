9a3ee4b1a579f36c22e9cde4e4d07ec29ca735ad
Adam Shannon <adamkshannon@gmail.com>
2016-09-13 18:00:18 -0500

    certs/remove: Making notes for osx

---
+++ certs/remove_darwin.go
@@ -0,0 +1,18 @@
+// +build darwin
+
+// security remove-trusted-cert <crt-file>
+// exit code 1 and stderr of:
+//  'SecTrustSettingsRemoveTrustSettings: The specified item could not be found in the keychain.'
+// if the cert isn't in the keychain
+//
+// otherwise, it will open an alert box for the password
+// is there a way to batch up removals?
+//
+// or, how does that work when go is exec'ing out?
+
+// security add-trusted-cert <crt-file>
+// works.
+
+// Is there a way to disable a cert? aka mark it as "Never Trust"?
+// Otherwise, we'll need to make a full backup of all certs before touching anything.
+// ^ Then offer a way to mass-import all of them.
