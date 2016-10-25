960264c533b74659fe3e245f243d97bdfbc97bba
Adam Shannon <adamkshannon@gmail.com>
2016-10-25 14:49:14 -0500

    build/linux: Rename build.sh -> run.sh

--- build/linux/run.sh
+++ build/linux/run.sh
@@ -4,6 +4,6 @@ set -e
 
 cd ./build/linux/
 cp ../../bin/cert-manage-linux cert-manage
-docker build -t cert-manage-linux:latest . > build.log
+docker build -t cert-manage-linux:latest . > run.log
 docker run -it cert-manage-linux:latest $@
 cd - > /dev/null
