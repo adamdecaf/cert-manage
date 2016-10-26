4a7b3596d48c22a0229170ea8614d1c4e7772a73
Adam Shannon <adamkshannon@gmail.com>
2016-10-26 07:55:56 -0500

    certs/darwin: Use full path for `security` binary

--- certs/find_darwin.go
+++ certs/find_darwin.go
@@ -8,7 +8,7 @@ import (
 )
 
 func FindCerts() ([]*x509.Certificate, error) {
-	b, err := exec.Command("security", "find-certificate", "-a", "-p").Output()
+	b, err := exec.Command("/usr/bin/security", "find-certificate", "-a", "-p").Output()
 	if err != nil {
 		return nil, err
 	}
