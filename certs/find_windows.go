61771d6207e3d9990339677f41cce8c898dd241d
Adam Shannon <adamkshannon@gmail.com>
2016-10-26 07:56:14 -0500

    certs/windows: Adding notes for how to list certs

--- certs/find_windows.go
+++ certs/find_windows.go
@@ -7,6 +7,11 @@ import (
 	// "os/exec"
 )
 
+// call into
+// https://msdn.microsoft.com/en-us/library/e78byta0(v=vs.110).aspx
+
+// To list certs, run `certmgr.msc` in a prompt..so os/exec ?
+
 func FindCerts() ([]*x509.Certificate, error) {
 	return nil, nil
 }
