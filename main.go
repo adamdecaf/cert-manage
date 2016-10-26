ae40421087c0959182936780e9d46dd32e32a03f
Adam Shannon <adamkshannon@gmail.com>
2016-10-26 11:18:13 -0500

    cmd: Adding version flag

--- main.go
+++ main.go
@@ -11,13 +11,21 @@ var (
 	// actions
 	find = flag.Bool("find", false, "Find certs for application or platform.")
 	whitelist = flag.String("whitelist", "", "Deactivate all certs except for those in the given whitelist")
+	version = flag.Bool("version", false, "Output the version number")
 
 	// where?
 	app = flag.String("app", "", "Find certs for an application (optional)")
 )
 
+const Version = "0.0.1-dev"
+
 func main() {
 	flag.Parse()
+
+	if version != nil && *version {
+		fmt.Println(Version)
+		return
+	}
 
 	// determine action
 	if find != nil && *find {
