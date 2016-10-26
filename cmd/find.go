452ebe3013e1c45d1d3af3b1a98cd2862edcd21f
Adam Shannon <adamkshannon@gmail.com>
2016-10-26 13:57:43 -0500

    cmd/find: print the fingerprint instead of malformed signature

--- cmd/find.go
+++ cmd/find.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"crypto/sha256"
 	"crypto/x509"
+	"encoding/hex"
 	"fmt"
 	"github.com/adamdecaf/cert-manage/certs"
 	"os"
@@ -30,4 +32,50 @@ func Find(app *string) {
 	}
 
 	printCertsToStdout(certificates)
+}
+
+// printCertsToStdout very verbosly prints out the ecah certificate's information
+// to stdout. This isn't very useful for machine parsing or small screen displays.
+func printCertsToStdout(certs []*x509.Certificate) {
+	for i := range certs[:1] {
+		fmt.Printf("cert\n")
+
+		ss := sha256.New()
+		ss.Write(certs[i].RawSubjectPublicKeyInfo)
+		fingerprint := hex.EncodeToString(ss.Sum(nil))
+
+		fmt.Printf("  sha256 fingerprint - %s\n", fingerprint)
+		fmt.Printf("  signature algorithm: %s\n", certs[i].SignatureAlgorithm.String())
+		fmt.Printf("  pub key algo - %d\n", certs[i].PublicKeyAlgorithm)
+		fmt.Printf("  Issuer CommonName - %s, SerialNumber - %s\n", certs[i].Issuer.CommonName, certs[i].Issuer.SerialNumber)
+		fmt.Printf("  Subject CommonName - %s, SerialNumber - %s\n", certs[i].Subject.CommonName, certs[i].Subject.SerialNumber)
+		fmt.Printf("  NotBefore - %s, NotAfter - %s\n", certs[i].NotBefore, certs[i].NotAfter)
+		fmt.Printf("  IsCA - %t\n", certs[i].IsCA)
+		fmt.Printf("  MaxPathLen - %d\n", certs[i].MaxPathLen)
+
+		fmt.Printf("  DNSNames\n")
+		for j := range certs[i].DNSNames {
+			fmt.Printf("    %s\n", certs[i].DNSNames[j])
+		}
+
+		fmt.Printf("  EmailAddresses\n")
+		for j := range certs[i].EmailAddresses {
+			fmt.Printf("    %s\n", certs[i].EmailAddresses[j])
+		}
+
+		fmt.Printf("  IPAddresses\n")
+		for j := range certs[i].IPAddresses {
+			fmt.Printf("    %s\n", certs[i].IPAddresses[j])
+		}
+
+		fmt.Printf("  PermittedDNSDomains\n")
+		for j := range certs[i].PermittedDNSDomains {
+			fmt.Printf("    %s\n", certs[i].PermittedDNSDomains[j])
+		}
+
+		fmt.Printf("  CRLDistributionPoints\n")
+		for j := range certs[i].CRLDistributionPoints {
+			fmt.Printf("    %s\n", certs[i].CRLDistributionPoints[j])
+		}
+	}
 }
