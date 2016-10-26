8a1160d1dd49a158636af772d9e9ae6317cf7875
Adam Shannon <adamkshannon@gmail.com>
2016-10-26 08:24:19 -0500

    certs: Fixing error message for finding app certs

--- certs/find.go
+++ certs/find.go
@@ -6,15 +6,18 @@ import (
 	"strings"
 )
 
+// todo: desc
 func FindCertsForApplication(app string) ([]*x509.Certificate, error) {
 	var certs []*x509.Certificate
-	err := fmt.Errorf("app %s not found", app)
+	var err error
 
 	switch strings.ToLower(app) {
 	case "chrome":
 		certs, err = FindCertsChrome()
 	case "java":
 		certs, err = FindCertsJava()
+	default:
+		err = fmt.Errorf("application '%s' not found", app)
 	}
 
 	return certs, err
