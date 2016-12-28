492e1cb445601d045105ed4f05932beda1b1a5fb
Adam Shannon <adamkshannon@gmail.com>
2016-12-28 16:09:18 -0600

    cmd: consolidate find into one file...

--- cmd/find.go
+++ cmd/find.go
@@ -8,6 +8,18 @@ import (
 	"strings"
 )
 
+// `FindCertsForPlatform` finds certs for the given platform.
+// The supported platforms can be found in the readme. They're compiled in
+// with build flags in the `certs/find_*.go` files.
+func FindCertsForPlatform(app *string, format string) {
+	certificates, err := certs.FindCerts()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	PrintCerts(certificates, format)
+}
+
 // `FindCertsForApp` finds certs for the given app.
 // The supported applications are listed in the readme. This includes
 // non-traditional applications like NSS.
