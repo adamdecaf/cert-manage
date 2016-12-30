c7fed6796324d2788ec5f164f3fbfd69632f0944
Adam Shannon <adamkshannon@gmail.com>
2016-12-30 13:25:46 -0600

    certs: doc on Keep()

--- certs/keep.go
+++ certs/keep.go
@@ -4,7 +4,9 @@ import (
 	"crypto/x509"
 )
 
-// ``
+// `Keep` filters a list of x509 Certificates against whitelist items to
+// retain only the certificates that are allowed by our whitelist.
+// An empty slice of certificates is a possible (and valid) output.
 func Keep(incoming []*x509.Certificate, whitelisted []WhitelistItem) []*x509.Certificate {
 	// Pretty bad search right now.
 	var keep []*x509.Certificate
@@ -17,3 +19,7 @@ func Keep(incoming []*x509.Certificate, whitelisted []WhitelistItem) []*x509.Cer
 	}
 	return keep
 }
+
+// todo: dedup certs already added by one whitelist item
+// e.g. If my []WhitelistItem contains a signature and Issuer.CommonName match
+// don't add the cert twice
