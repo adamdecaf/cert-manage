1a5528929b6608f5ecc94b9eb9724a5aeb44a4bc
Adam Shannon <adamkshannon@gmail.com>
2017-01-11 17:31:55 -0600

    certs: typo in test

--- certs/filter_test.go
+++ certs/filter_test.go
@@ -24,7 +24,7 @@ func TestFilter_EmptyWhitelist(t *testing.T) {
 }
 
 func TestFilter_Remove(t *testing.T) {
-	certificates := loadPEM(t, "../testdata/lots.crt")
+	certificates := loadPEM(t, "../testdata/example.crt")
 	signature := "96940d991419151450d1e75f66218f6f2594e1df4af31a5ad673c9a8746817ce"
 
 	wh := []WhitelistItem{HexSignatureWhitelistItem{Signature: signature}}
