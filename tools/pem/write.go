59a4d9f02fb624c1dec084a587b522786ae195ad
Adam Shannon <adamkshannon@gmail.com>
2017-10-12 20:11:22 -0500

    tools/pem: support for writing back a series of certs in PEM format

---
+++ tools/pem/write.go
@@ -0,0 +1,30 @@
+package pem
+
+import (
+	"bytes"
+	"crypto/x509"
+	p "encoding/pem"
+	"io/ioutil"
+	"os"
+)
+
+func ToFile(path string, certs []*x509.Certificate) error {
+	var perms os.FileMode = 0666
+	stat, err := os.Stat(path)
+	if err == nil {
+		perms = stat.Mode()
+	}
+
+	// write the cert(s)
+	var buf bytes.Buffer
+	for i := range certs {
+		b := &p.Block{
+			Type:  "CERTIFICATE",
+			Bytes: certs[i].Raw,
+		}
+		if err := p.Encode(&buf, b); err != nil {
+			return err
+		}
+	}
+	return ioutil.WriteFile(path, buf.Bytes(), perms)
+}
