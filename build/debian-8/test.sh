7a2cf183a2e7b84249e0c96bf27d22b195a32eb4
Adam Shannon <adamkshannon@gmail.com>
2017-10-14 17:12:39 -0500

    store/linux: support Backup()
    Issue: https://github.com/adamdecaf/cert-manage/issues/7

--- build/debian-8/test.sh
+++ build/debian-8/test.sh
@@ -5,16 +5,27 @@ cd build/debian-8/
 cp ../../bin/cert-manage-linux-amd64 cert-manage
 cp ../../testdata/globalsign-whitelist.json whitelist.json
 
+total=179
+after=6
 cat > main <<EOF
 #!/bin/sh
 set -e
 
 # Verify we're starting with the correct number of certs
-/bin/cert-manage -list | wc -l | grep 175
+/bin/cert-manage -list | wc -l | grep $total
+
+# Make a backup
+/bin/cert-manage -backup
+
+# Quick check
+ls -l /usr/share/ca-certificates/* | wc -l | grep $total
+ls -l /usr/share/ca-certificates.backup/* | wc -l | grep $total
 
 # Whitelist and verify
 /bin/cert-manage -whitelist /whitelist.json
-/bin/cert-manage -list | wc -l | grep 6
+/bin/cert-manage -list | wc -l | grep $after
+
+# Restore
 EOF
 
 chmod +x main
