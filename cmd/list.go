9c6f7d25e3f7d4a89e0cf63c9b67040701b5e870
Adam Shannon <adamkshannon@gmail.com>
2017-10-14 16:56:17 -0500

    offer -backup as a flag now
    -backup can be used in conjunction with -list and -whitelist. If
     provided a backup will be performed before either operation.

--- cmd/list.go
+++ cmd/list.go
@@ -10,19 +10,20 @@ import (
 // ListCertsForPlatform finds certs for the given platform.
 // The supported platforms can be found in the readme. They're compiled in
 // with build flags in the `certs/find_*.go` files.
-func ListCertsForPlatform(format string) {
+func ListCertsForPlatform(format string) error {
 	certificates, err := store.Platform().List()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	printCerts(certificates, format)
+	return nil
 }
 
 // ListCertsForApp finds certs for the given app.
 // The supported applications are listed in the readme. This includes
 // non-traditional applications like NSS.
-func ListCertsForApp(app string, format string) {
+func ListCertsForApp(app string, format string) error {
 	st, err := store.ForApp(app)
 	if err != nil {
 		fmt.Println(err)
@@ -43,4 +44,5 @@ func ListCertsForApp(app string, format string) {
 
 	// Output the certificates
 	printCerts(certificates, format)
+	return nil
 }
--- main.go
+++ main.go
@@ -17,11 +17,10 @@ var (
 	fs = flag.NewFlagSet("flags", flag.ExitOnError)
 
 	// Commands
+	backup    = fs.Bool("backup", false, "Make a backup")
 	list      = fs.Bool("list", false, "List certificates (by default on the system, see -app)")
 	whitelist = fs.String("whitelist", "", "Filter certificates according to the provided whitelist")
-	// TODO(adam): future commands
-	// - backup
-	// - restore
+	// TODO(adam): restore
 
 	// Filters
 	app = fs.String("app", "", "Specify an application (see -list)")
@@ -34,28 +33,63 @@ var (
 func main() {
 	fs.Parse(os.Args[1:])
 
-	wh := strings.TrimSpace(*whitelist)
-	if whitelist != nil && wh != "" {
-		if app != nil && *app != "" {
-			err := cmd.WhitelistForApp(*app, *whitelist, *format)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-		err := cmd.WhitelistForPlatform(*whitelist, *format)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	// Take a backup
+	// Note: This always needs to be done first
+	if backup != nil && *backup {
+		err := appChoice(app,
+			func(a string) error {
+				return cmd.BackupForApp(a)
+			},
+			func() error {
+				return cmd.BackupForPlatform()
+			})
+		exit("Backup completed successfully", err)
 	}
 
+	// Whitelist
+	wh := strings.TrimSpace(*whitelist)
+	if whitelist != nil && wh != "" {
+		err := appChoice(app,
+			func(a string) error {
+				return cmd.WhitelistForApp(a, *whitelist, *format)
+			},
+			func() error {
+				return cmd.WhitelistForPlatform(*whitelist, *format)
+			})
+		exit("Whitelist completed successfully", err)
+	}
+
+	// List
 	if list != nil && *list {
-		if app != nil && *app != "" {
-			cmd.ListCertsForApp(*app, *format)
-			return
-		}
-		cmd.ListCertsForPlatform(*format)
-		return
+		err := appChoice(app,
+			func(a string) error {
+				return cmd.ListCertsForApp(*app, *format)
+			},
+			func() error {
+				return cmd.ListCertsForPlatform(*format)
+			})
+		exit("", err)
+	}
+}
+
+type fn func() error
+type appfn func(string) error
+
+// appChoice decides between `appfn` and `fn` given the presence of `app`
+// being non-nil and a non-empty string.
+func appChoice(app *string, appfn appfn, fn fn) error {
+	if app != nil && *app != "" {
+		return appfn(*app)
+	}
+	return fn()
+}
+
+func exit(msg string, err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if msg != "" {
+		fmt.Println(msg)
 	}
 }
