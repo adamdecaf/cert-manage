98a6519d1a8fd6ff3d24407f439921bf82c7f17e
Adam Shannon <adamkshannon@gmail.com>
2017-10-19 18:03:18 -0500

    tools/_x509: wow, generate fingerprints correctly

--- tools/_x509/x509.go
+++ tools/_x509/x509.go
@@ -1,16 +1,21 @@
 package _x509
 
 import (
+	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/hex"
 )
 
-// https://tools.ietf.org/html/rfc7250
+func GetHexSHA1Fingerprint(c x509.Certificate) string {
+	ss := sha1.New()
+	ss.Write(c.Raw)
+	return hex.EncodeToString(ss.Sum(nil))
+}
+
 func GetHexSHA256Fingerprint(c x509.Certificate) string {
-	// Grab the cert's hex encoding
 	ss := sha256.New()
-	ss.Write(c.RawSubjectPublicKeyInfo)
+	ss.Write(c.Raw)
 	return hex.EncodeToString(ss.Sum(nil))
 }
 
