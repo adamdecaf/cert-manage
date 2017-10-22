2d7ac8712379e144afa97afb0013c98b8cfaff8b
Adam Shannon <adamkshannon@gmail.com>
2017-10-22 15:04:33 -0500

    build: show last log after failure

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Run each build's tests
 platforms=(alpine-36 debian-8 debian-9 ubuntu-1604 ubuntu-1704)
@@ -7,4 +6,9 @@ for plat in "${platforms[@]}"
 do
     echo "CI: $plat"
     ./build/"$plat"/test.sh
+    if [ ! $? -eq 0 ];
+    then
+        cat ./build/"$plat"/test.log
+        exit 1
+    fi
 done
