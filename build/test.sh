8ca772b8da44b44df5a956ba10c6d9c261d88786
Adam Shannon <adamkshannon@gmail.com>
2017-10-22 15:43:49 -0500

    build: skip docker ci tests on osx

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Travis-ci doesn't support docker..
+# https://docs.travis-ci.com/user/docker/
+if [ "$TRAVIS_OS_NAME" == "osx" ];
+then
+    echo "TravisCI currently does not support docker on osx, skipping tests"
+    exit 0
+fi
+
 # Run each build's tests
 platforms=(alpine-36 debian-8 debian-9 ubuntu-1604 ubuntu-1704)
 for plat in "${platforms[@]}"
