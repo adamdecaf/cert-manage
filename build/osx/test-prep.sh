e1694cb463ca7c30db25b5670e60257f712b009a
Adam Shannon <adamkshannon@gmail.com>
2017-11-03 21:35:06 -0500

    build/osx: only try and setup keychain if we're online

--- build/osx/test-prep.sh
+++ build/osx/test-prep.sh
@@ -2,4 +2,8 @@
 set -e
 
 # Make a request to try and setup the Keychain
-curl -O /dev/null https://google.com
+ssid=$(networksetup -getairportnetwork en0 | cut -c 24-)
+if [ -n "$ssid" ];
+then
+    curl -s -o /dev/null https://google.com
+fi
