d96200e9853a5ad98ffbeff5717fe45a363f445a
Adam Shannon <adamkshannon@gmail.com>
2017-11-04 12:52:30 -0500

    build/osx: only actually exec test-prep.sh on Darwin

--- build/osx/test-prep.sh
+++ build/osx/test-prep.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# Make a request to try and setup the Keychain
-ssid=$(networksetup -getairportnetwork en0 | cut -c 24-)
-if [ -n "$ssid" ];
+if [[ `uname -s` == 'Darwin' ]];
 then
-    curl -s -o /dev/null https://google.com
+    # Make a request to try and setup the Keychain
+    ssid=$(networksetup -getairportnetwork en0 | cut -c 24-)
+    if [ -n "$ssid" ];
+    then
+        curl -s -o /dev/null https://google.com
+    fi
 fi
