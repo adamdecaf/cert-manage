0255d13b3e5b1a7a2080b44e4af28e9465b479ac
Adam Shannon <adamkshannon@gmail.com>
2017-11-05 15:09:43 -0600

    store/linux: fix from go vet

--- store/linux_test.go
+++ store/linux_test.go
@@ -11,6 +11,6 @@ func StoreLinux__cadir(t *testing.T) {
 	// just grab the linuxStore and make sure it has a cadir member
 	s := platform()
 	if s.ca == nil || s.ca.empty() {
-		t.Error("no cadir found on platform: %s", runtime.GOOS)
+		t.Errorf("no cadir found on platform: %s", runtime.GOOS)
 	}
 }
