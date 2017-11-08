5d5382c36e5d96a1204c836b91b21aa79d49ef40
Adam Shannon <adamkshannon@gmail.com>
2017-11-08 16:19:50 -0600

    tools/file: adding sorting tests

--- tools/file/sort_test.go
+++ tools/file/sort_test.go
@@ -1,7 +1,7 @@
 package file
 
 import (
-	"reflect"
+	"io/ioutil"
 	"sort"
 	"testing"
 )
@@ -10,7 +10,44 @@ func TestFile__sortStrings(t *testing.T) {
 	strings := []string{"b", "A", "C", "a", " ", ""}
 	sort.Sort(iStringSlice(strings))
 	answer := []string{"", " ", "A", "a", "b", "C"}
-	if !reflect.DeepEqual(strings, answer) {
-		t.Fatalf("sorted strings don't equal answer, strings=%v", strings)
+
+	for i := range strings {
+		if strings[i] != answer[i] {
+			t.Fatalf("idx: %d didn't match, %s vs %s", i, strings[i], answer[i])
+		}
+	}
+}
+
+func TestFile__sortNames(t *testing.T) {
+	names := []string{"b", "A", "C", "a", " ", ""}
+	sort.Sort(iStringSlice(names))
+	answer := []string{"", " ", "A", "a", "b", "C"}
+
+	for i := range names {
+		if names[i] != answer[i] {
+			t.Fatalf("idx: %d didn't match, %s vs %s", i, names[i], answer[i])
+		}
+	}
+}
+
+func TestFile__sortFileInfos(t *testing.T) {
+	fds, err := ioutil.ReadDir(".")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	answers := []string{
+		"file.go",
+		"file_test.go",
+		"file_unix.go",
+		"file_windows.go",
+		"sort.go",
+		"sort_test.go",
+	}
+
+	for i := range fds {
+		if fds[i].Name() != answers[i] {
+			t.Fatalf("idx: %d didn't match, %s vs %s", i, fds[i].Name(), answers[i])
+		}
 	}
 }
