40a62d11e50d7cd2a44156f99089bab28fa074dd
Adam Shannon <adamkshannon@gmail.com>
2017-11-08 07:35:00 -0600

    build: show the test log on failures

--- build/test.sh
+++ build/test.sh
@@ -20,10 +20,12 @@ platforms=(alpine-36 debian-8 debian-9 ubuntu-1604 ubuntu-1704)
 for plat in "${platforms[@]}"
 do
     echo "CI: $plat"
+    set +e
     ./build/"$plat"/test.sh
     if [ ! $? -eq 0 ];
     then
         cat ./build/"$plat"/test.log
         exit 1
     fi
+    set -e
 done
