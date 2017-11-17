09068c5469a1771f127c19d22190ee1387f102f8
Adam Shannon <adamkshannon@gmail.com>
2017-11-17 16:59:36 -0600

    build/osx: we need to test-prep on travis

--- build/osx/test-prep.sh
+++ build/osx/test-prep.sh
@@ -5,8 +5,10 @@ if [[ `uname -s` == 'Darwin' ]];
 then
     # Make a request to try and setup the Keychain
     ssid=$(networksetup -getairportnetwork en0 | cut -c 24-)
-    if [ -n "$ssid" ];
+    if [[ -n "$ssid" || -n "$TRAVIS_OS_NAME" ]];
     then
+        set -x
         curl -s -o /dev/null https://google.com
+        set +x
     fi
 fi
