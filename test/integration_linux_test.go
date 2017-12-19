f9ee4dee95ea05e551b498ecf76f5ebfd76cdb46
Adam Shannon <adamkshannon@gmail.com>
2017-12-19 10:08:43 -0600

    test: try and fix linux date test

--- test/integration_linux_test.go
+++ test/integration_linux_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestIntegration__date(t *testing.T) {
-	cmd := Command("date", "--date='@0'").Trim()
+	cmd := Command("date", "--date", "'@0'").Trim()
 	cmd.EqualT(t, "Thu Jan  1 00:00:00 UTC 1970")
 }
