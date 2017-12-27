0837e74df1885fe046a4a9f8daaacc3bfec7b08a
Adam Shannon <adamkshannon@gmail.com>
2017-12-27 17:53:28 -0600

    store/darwin: mock out darwin by default, use impl under DEBUG
    Issue: https://github.com/adamdecaf/cert-manage/issues/9

---
+++ store/empty.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"crypto/x509"
+	"fmt"
+	"os"
+
+	"github.com/adamdecaf/cert-manage/whitelist"
+)
+
+// emptyStore represents a Store which has no implementation
+// This is useful for stubs
+type emptyStore struct{}
+
+func (s emptyStore) printNotce() {
+	fmt.Fprintln(os.Stderr, "NOTICE: This implementation is currently stubbed, nothing is happening.")
+}
+
+func (s emptyStore) Backup() error {
+	s.printNotce()
+	return nil
+}
+func (s emptyStore) List() ([]*x509.Certificate, error) {
+	s.printNotce()
+	return nil, nil
+}
+func (s emptyStore) Remove(whitelist.Whitelist) error {
+	s.printNotce()
+	return nil
+}
+func (s emptyStore) Restore(where string) error {
+	s.printNotce()
+	return nil
+}
