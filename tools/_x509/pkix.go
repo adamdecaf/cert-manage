b2e5a3c3afb3712b2c3053adeb66b20105ce9e2c
Adam Shannon <adamkshannon@gmail.com>
2017-12-31 14:43:12 -0600

    tools/_x509: Add test on StringifyPKIXName

--- tools/_x509/pkix.go
+++ tools/_x509/pkix.go
@@ -6,6 +6,7 @@ import (
 	"crypto/x509/pkix"
 )
 
+// TODO(adam): Replace with RDSSquence.String() in g1.10
 func StringifyPKIXName(name pkix.Name) string {
 	if len(name.OrganizationalUnit) > 0 {
 		return fmt.Sprintf("%s, %s", strings.Join(name.Organization, " "), name.OrganizationalUnit[0])
---
+++ tools/_x509/pkix_test.go
@@ -0,0 +1,33 @@
+package _x509
+
+import (
+	"testing"
+
+	"github.com/adamdecaf/cert-manage/tools/pem"
+)
+
+func Test509__StringifyPKIXName(t *testing.T) {
+	certs, err := pem.FromFile("../../testdata/lots.crt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(certs) == 0 {
+		t.Fatal("got no certs")
+	}
+
+	answers := map[int]string{
+		0: "Entrust.net, www.entrust.net/CPS_2048 incorp. by ref. (limits liab.)",
+		1: "Entrust, Inc., See www.entrust.net/legal-terms",
+		2: "Entrust, Inc., See www.entrust.net/legal-terms",
+		3: "Entrust, Inc., www.entrust.net/CPS is incorporated by reference",
+		4: "AS Sertifitseerimiskeskus",
+	}
+
+	for i := range certs {
+		out := StringifyPKIXName(certs[i].Subject)
+		ans := answers[i]
+		if out != ans {
+			t.Errorf("idx %d, got '%s'", i, out)
+		}
+	}
+}
