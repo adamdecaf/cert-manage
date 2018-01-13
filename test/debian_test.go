4064023911d6e13f4de74795388ee39bdc25bba3
Adam Shannon <adamkshannon@gmail.com>
2018-01-12 22:00:04 -0600

    test/debian: switch over to suite, run java also

--- test/debian_test.go
+++ test/debian_test.go
@@ -1,24 +1,13 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestDebian(t *testing.T) {
-	total, after := "166", "5" // os specific values, will change
-
 	img := Dockerfile("envs/debian")
-	img.CertManage("list", "-count", "|", "grep", total)
-	// Backup
-	img.CertManage("backup")
-	img.RunSplit(fmt.Sprintf("ls -1 /usr/share/ca-certificates/* | wc -l | grep %s", total))
-	img.RunSplit(fmt.Sprintf("ls -1 /usr/share/ca-certificates.backup/* | wc -l | grep %s", total))
-	// Whitelist
-	img.CertManage("whitelist", "-file", "/whitelist.json")
-	img.CertManage("list", "-count", "|", "grep", after)
-	// Restore
-	img.CertManage("restore")
-	img.CertManage("list", "-count", "|", "grep", total)
-	img.SuccessT(t)
+	linuxSuite(t, img, "166", "5")
+
+	img = Dockerfile("envs/debian")
+	javaSuite(t, img, "166", "12")
 }
