ffbe55f247e86c3b8bdc8d5ce6bf6ca017060b42
Adam Shannon <adamkshannon@gmail.com>
2018-01-13 15:25:04 -0600

    pkg/ui: support open on linux via xdg-open

--- pkg/ui/open_stub.go
+++ pkg/ui/open_stub.go
@@ -1,13 +1,31 @@
-// +build linux windows
-
 package ui
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
+
+	"github.com/adamdecaf/cert-manage/pkg/ui/server"
 )
 
 func Open() error {
+	var command string
+	switch runtime.GOOS {
+	case "darwin":
+		command = "open"
+	case "linux":
+		command = "xdg-open"
+	}
+
+	if command != "" {
+		cmd := exec.Command(command, server.Address())
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("ERROR: while loading ui, err=%v\n", err)
+		}
+		return err
+	}
+
 	fmt.Printf("WARN: ui not supported on %s yet\n", runtime.GOOS)
 	return nil
 }
