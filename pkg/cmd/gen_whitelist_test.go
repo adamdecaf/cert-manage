15df6300c1cbe575bfbbcdcf5b2f8c2f96698018
Adam Shannon <adamkshannon@gmail.com>
2018-01-15 18:09:18 -0600

    pkg/cmd: test out gen whitelist accumulator
    Issue: https://github.com/adamdecaf/cert-manage/issues/5

--- pkg/cmd/gen_whitelist_test.go
+++ pkg/cmd/gen_whitelist_test.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"errors"
+	"net/url"
 	"reflect"
+	"strings"
+	"sync"
 	"testing"
 )
 
@@ -28,5 +32,48 @@ func TestGenWhitelist_getChoices(t *testing.T) {
 	choices = getChoices("browser,file", "")
 	if !reflect.DeepEqual(choices, []string{"browser", "file"}) {
 		t.Errorf("got %q", choices)
+	}
+}
+
+func TestGenWhitelist_accumulateUrls(t *testing.T) {
+	uacc := make(chan []*url.URL)
+	eacc := make(chan error)
+	wg := sync.WaitGroup{}
+
+	// happy path
+	fn := func() ([]*url.URL, error) {
+		defer wg.Done()
+		u, _ := url.Parse("https://github.com")
+		return []*url.URL{u}, nil
+	}
+	wg.Add(1)
+	go accumulateUrls(fn, uacc, eacc)
+	wg.Wait() // just so we ensure the code finished
+
+	select {
+	case urls := <-uacc:
+		if len(urls) != 1 {
+			t.Errorf("got %q", urls)
+		}
+	case err := <-eacc:
+		t.Error(err)
+	}
+
+	// unhappy path
+	fn = func() ([]*url.URL, error) {
+		defer wg.Done()
+		return nil, errors.New("whoops")
+	}
+	wg.Add(1)
+	go accumulateUrls(fn, uacc, eacc)
+	wg.Wait() // just so we ensure the code finished
+
+	select {
+	case urls := <-uacc:
+		t.Errorf("expected error, got %q", urls)
+	case err := <-eacc:
+		if !strings.Contains(err.Error(), "whoops") {
+			t.Errorf("got %q", err)
+		}
 	}
 }
