21826b416bcbe9eb8c98353303af314f57f962db
Adam Shannon <adamkshannon@gmail.com>
2018-01-15 18:32:54 -0600

    pkg/whitelist/gen: add a basic worker pool
    Issue: https://github.com/adamdecaf/cert-manage/issues/5

--- pkg/whitelist/gen/gen.go
+++ pkg/whitelist/gen/gen.go
@@ -8,3 +8,23 @@ import (
 func FindCACertificates([]*url.URL) ([]*x509.Certificate, error) {
 	return nil, nil
 }
+
+type gate struct {
+	c chan struct{}
+}
+func (g gate) begin() {
+	g.c <- struct{}{}
+}
+func (g gate) done() {
+	select {
+	case <-g.c:
+	default:
+		panic("invalid state")
+	}
+}
+func newgate(n int) *gate {
+	return &gate{
+		c: make(chan struct{}, n),
+	}
+}
+
---
+++ pkg/whitelist/gen/gen_test.go
@@ -0,0 +1,58 @@
+package gen
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGen__gate(t *testing.T) {
+	workers := newgate(2)
+
+	// three timeslots, easy to fill in
+	var w1 time.Time
+	var w2 time.Time
+	var w3 time.Time
+
+	// start workers
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	// first
+	workers.begin()
+	go func() {
+		w1 = time.Now()
+		time.Sleep(100 * time.Millisecond)
+		workers.done()
+		wg.Done()
+	}()
+
+	// second
+	workers.begin()
+	go func() {
+		w2 = time.Now()
+		time.Sleep(100 * time.Millisecond)
+		workers.done()
+		wg.Done()
+	}()
+
+	// third
+	go func() {
+		workers.begin()
+		w3 = time.Now()
+		workers.done()
+		wg.Done()
+	}()
+
+	wg.Wait()
+	// check (w3 should come last)
+	if w1.After(w3) || w2.After(w3) {
+		t.Errorf("w1=%q > w3=%q", w1, w3)
+	}
+	if w2.After(w3) {
+		t.Errorf("w2=%q > w3=%q", w2, w3)
+	}
+	if w1.IsZero() || w2.IsZero() || w3.IsZero() {
+		t.Errorf("can't be zero: w1=%q, w2=%q, w3=%q", w1, w2, w3)
+	}
+}
