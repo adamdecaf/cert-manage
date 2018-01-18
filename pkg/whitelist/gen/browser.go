3155856a32ca4baae1f0aa330da78e60953df1a7
Adam Shannon <adamkshannon@gmail.com>
2018-01-17 20:57:28 -0600

    pkg/whitelist/gen: start reading from moz_places
    Issue: https://github.com/adamdecaf/cert-manage/issues/108

--- pkg/whitelist/gen/browser.go
+++ pkg/whitelist/gen/browser.go
@@ -1,7 +1,10 @@
 package gen
 
 import (
+	"fmt"
 	"net/url"
+
+	"github.com/go-sqlite/sqlite3"
 )
 
 // TODO(adam): impl
@@ -12,5 +15,24 @@ func FromAllBrowsers() ([]*url.URL, error) {
 
 // TODO(adam): impl
 func FromBrowser(name string) ([]*url.URL, error) {
-	return nil, nil
+	db, err := sqlite3.Open("/Users/adam/Library/Application Support/Firefox/Profiles/rrdlhe7o.default/places.sqlite")
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.VisitTableRecords("moz_places", func(rowId *int64, rec sqlite3.Record) error {
+		if rowId == nil {
+			return fmt.Errorf("unexpected nil RowID in Chrome sqlite database")
+		}
+
+		url, ok := rec.Values[1].(string)
+		if !ok {
+			fmt.Println(rec.Values)
+		}
+		fmt.Println(url)
+
+		return nil
+	})
+
+	return nil, err
 }
