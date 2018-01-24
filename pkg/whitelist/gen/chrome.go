aa5b829217ca414e5ec59f0f5e4146fb89630769
Adam Shannon <adamkshannon@gmail.com>
2018-01-23 18:48:03 -0600

    whitelist/gen: todo for chrome paths, needs testing

--- pkg/whitelist/gen/chrome.go
+++ pkg/whitelist/gen/chrome.go
@@ -13,7 +13,12 @@ import (
 var (
 	chromeProfileLocations = []string{
 		filepath.Join(file.HomeDir(), `/Library/Application Support/Google/Chrome/Default/History`), // OSX not signed in
-		// C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Preferences // TODO
+		// TODO(adam):
+		// Linux: /home/$USER/.config/google-chrome/
+		// Linux: /home/$USER/.config/chromium/
+		// Windows Vista (and Win 7): C:\Users\[USERNAME]\AppData\Local\Google\Chrome\
+		// Windows XP: C:\Documents and Settings\[USERNAME]\Local Settings\Application Data\Google\Chrome\
+		// Win 8+: C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Preferences
 	}
 )
 
