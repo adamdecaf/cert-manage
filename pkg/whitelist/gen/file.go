bc4788f37624f8f483385d4c90975214c7acc6f8
Adam Shannon <adamkshannon@gmail.com>
2018-03-11 16:15:06 -0500

    whitelist/gen: Support reading "top n domains" csv files
    Allow them to be gzipped. Scan each columns data and use the first url
    looking thing.
    
    Issue: https://github.com/adamdecaf/cert-manage/issues/156

--- pkg/whitelist/gen/file.go
+++ pkg/whitelist/gen/file.go
@@ -16,13 +16,20 @@ package gen
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/adamdecaf/cert-manage/pkg/file"
+)
+
+var (
+	gzipHeader = []byte{0x1f, 0x8b}
 )
 
 // FromFile reads a text file and grabs urls separated by newlines
@@ -41,15 +48,55 @@ func FromFile(path string) ([]*url.URL, error) {
 		return nil, err
 	}
 	var res []*url.URL
-	rdr := bufio.NewScanner(fd)
+	rdr := bufio.NewScanner(decodeIfGzipped(fd))
+
 	for rdr.Scan() {
-		line := strings.TrimSpace(rdr.Text())
-		if line != "" {
-			u, err := url.Parse(line)
-			if err == nil && u != nil {
-				res = append(res, u)
-			}
+		if u := findUrlInLine(rdr.Text()); u != nil {
+			res = append(res, u)
 		}
 	}
 	return res, nil
+}
+
+// decodeIfGzipped attempts to wrap the bufio.Reader if the header contains the gzip
+// magic header, otherwise the original reader is returned.
+func decodeIfGzipped(r io.Reader) io.Reader {
+	rdr := bufio.NewReader(r)
+	bs, err := rdr.Peek(2)
+	if err != nil {
+		// Probably not a good thing we error'd, probably io.EOF or something
+		return r
+	}
+	if bytes.Equal(bs, gzipHeader) {
+		rdr, err := gzip.NewReader(rdr)
+		if err != nil {
+			return r // don't wrap reader in gzip
+		}
+		return rdr
+	}
+	return rdr
+}
+
+// findUrlInLine attempts to find the first url embedded in a line
+// of plain text
+//
+// This would be from a file just containing URLS or a "top n domains"
+// from a service like alexa or cisco.
+//
+// e.g. 1,google.com
+func findUrlInLine(line string) *url.URL {
+	// Split on , -- usually from the "top n domains" files
+	parts := strings.Split(line, ",")
+	for i := range parts {
+		parts[i] = strings.TrimSpace(parts[i])
+		if parts[i] == "" {
+			continue
+		}
+		// return whatever parses as a URL
+		u, err := url.Parse(parts[i])
+		if err == nil {
+			return u
+		}
+	}
+	return nil
 }
